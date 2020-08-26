62504ce334f3f222e0d8f75cc32777f011e4e8fc
StevenJBurns <stevenjburns@msn.com>
2020-08-25 18:05:24 -0600

    chore: original CRA generated files; minor tweaks to public HTML file; update tsconfig to use absolute paths; new launch.json for React debugging

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from 'assets/react-logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Edit <code>src/App.tsx</code> and save to reload.</p>
+      <a className="App-link" href="https://reactjs.org" rel="noopener noreferrer" target="_blank">Learn React</a>
+    </header>
+  </div>
+);
+
+export default App;
